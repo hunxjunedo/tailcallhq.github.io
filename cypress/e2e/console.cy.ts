5650c745565cc9ca31e1ea6ddd0b09ab223c7092
= <=>
2024-09-26 16:53:37 +0500

    initial commit

---
+++ cypress/e2e/console.cy.ts
@@ -0,0 +1,12 @@
+describe('console checker', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000'); 
+  });
+
+  it('should not have console errors and warnings', () => {
+    cy.window().then(() => {
+      const consoleErrors = Cypress.env('consoleErrors') || [];
+      expect(consoleErrors).to.be.empty; // Ensure no console errors were logged
+    });
+  });
+});
---
+++ cypress/support/e2e.ts
@@ -0,0 +1,10 @@
+
+import './commands'
+
+Cypress.on('window:before:load', (win) => {
+    const pushArgs = (...args) => {
+        Cypress.env('consoleErrors', [...Cypress.env('consoleErrors')  || [], args]);
+    }
+    win.console.error = pushArgs
+    win.console.warn = pushArgs
+})
